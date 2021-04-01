20389348c62870f4fc04497e0a432931aa5cacf9
dpotekhin <d.potekhin@peppers-studio.ru>
2021-04-01 23:17:08 +0300

    fix(pDrawing): the calculation of path bounding box has been changed to Math

--- ps/pDrawing.js
+++ ps/pDrawing.js
@@ -220,38 +220,156 @@ ToDo:
 */
 pDrawing.prototype.getStrokesBox = function( _strokesLayers ){
   
+  var _this = this;
+
   // MessageLog.trace('pDrawing.getBox: '+JSON.stringify(_strokesLayers, true, '  ')+', '+JSON.stringify(_art, true, '  ')+', '+_frame );
 
   // if( !this.node ) return;
-
-  var _strokesLayers;
-
-  this.createTempDrawing(); // Switch the current Entry to temporary  
-  this.clearArtByDrawingId( true, this.tempDrawingId ); // Clear the temp Entry
-
-  var toolSettings = {
-    // drawing : {node: this.node, frame: this.validateFrame( _frame )}
-    // drawing  : this.selectedToolSettings.currentDrawing,
-    drawing: { elementId: this.elementId, drawingId: this.tempDrawingId }
-  };
 
   var resultBox;
 
   this.iterateArts(function(art){
 
-    toolSettings.art = art;
-    toolSettings.layers = _strokesLayers[art];
+    var artStokes = _strokesLayers[art];
+    // MessageLog.trace( art+' > '+JSON.stringify(artStokes,true,'  ') );
+    
 
-    DrawingTools.createLayers( toolSettings ); // Copy strokes to the temp Entry
-    var box = Drawing.query.getBox(toolSettings); // Get the Box of the temp Entry
-    // MessageLog.trace('box: '+art+' : '+JSON.stringify(box,true,' '));
-    resultBox = Utils.joinBoxes( resultBox, box );
+    artStokes.forEach(function(artStroke,artStroke_i){
+      // MessageLog.trace( art+' ) '+artStroke_i+' > '+JSON.stringify(artStroke.strokes,true,'  ') );
 
+      artStroke.strokes.forEach(function(stroke,stroke_i){
+        //MessageLog.trace( art+' ) '+artStroke_i+' > '+stroke.path.length );
+        
+        var currentPoint;
+
+        stroke.path.forEach(function(point,point_i){
+          
+          MessageLog.trace( art+' ) '+artStroke_i+' > '+JSON.stringify(point,true,'  ') );
+          
+          if( point.onCurve ){ // Start or End point
+
+            if( currentPoint ){ // The Point is not the very first
+
+              if( currentPoint.length == 1 ){ // a stright line
+                // MessageLog.trace('Line!!!');
+                resultBox = Utils.joinBoxes( resultBox, _this.getLineBox( currentPoint[0], point ) );
+
+              }else{ // a Bezier curve
+                var box = _this.getBezierBox( currentPoint[0], currentPoint[1], currentPoint[2], point );
+                // MessageLog.trace('Bezier: '+ JSON.stringify(box, true, '  ') );
+                resultBox = Utils.joinBoxes( resultBox, box );
+              }
+              
+            }
+
+            currentPoint = [point];
+
+          }else{
+            currentPoint.push(point);
+          }
+
+        });
+
+      });
+
+    })
+    
   });
 
-  this.clearArtByDrawingId( true, this.tempDrawingId ); // // Clear the temp Entry
-
   return resultBox;
+
+}
+
+
+
+//
+pDrawing.prototype.getLineBox = function( p0, p1 ) {
+  
+  var result = {};
+
+  if( p0.x < p1.x ){
+    result.x0 = p0.x;
+    result.x1 = p1.x;
+  }else{
+    result.x0 = p1.x;
+    result.x1 = p0.x;
+  }
+
+  if( p0.y < p1.y ){
+    result.y0 = p0.y;
+    result.y1 = p1.y;
+  }else{
+    result.y0 = p1.y;
+    result.y1 = p0.y;
+  }
+
+  return result;
+}
+
+
+//
+pDrawing.prototype.getBezierBox = function(p0, p1, p2, p3) {
+
+  var tvalues = [], xvalues = [], yvalues = [],
+      a, b, c, t, t1, t2, b2ac, sqrtb2ac;
+  for (var i = 0; i < 2; ++i) {
+      if (i == 0) {
+          b = 6 * p0.x - 12 * p1.x + 6 * p2.x;
+          a = -3 * p0.x + 9 * p1.x - 9 * p2.x + 3 * p3.x;
+          c = 3 * p1.x - 3 * p0.x;
+      } else {
+          b = 6 * p0.y - 12 * p1.y + 6 * p2.y;
+          a = -3 * p0.y + 9 * p1.y - 9 * p2.y + 3 * p3.y;
+          c = 3 * p1.y - 3 * p0.y;
+      }
+      if (Math.abs(a) < 1e-12) {
+          if (Math.abs(b) < 1e-12) {
+              continue;
+          }
+          t = -c / b;
+          if (0 < t && t < 1) {
+              tvalues.push(t);
+          }
+          continue;
+      }
+      b2ac = b * b - 4 * c * a;
+      if (b2ac < 0) {
+          if (Math.abs(b2ac) < 1e-12) {
+              t = -b / (2 * a);
+              if (0 < t && t < 1) {
+                  tvalues.push(t);
+              }
+          }
+          continue;
+      }
+      sqrtb2ac = Math.sqrt(b2ac);
+      t1 = (-b + sqrtb2ac) / (2 * a);
+      if (0 < t1 && t1 < 1) {
+          tvalues.push(t1);
+      }
+      t2 = (-b - sqrtb2ac) / (2 * a);
+      if (0 < t2 && t2 < 1) {
+          tvalues.push(t2);
+      }
+  }
+
+  var j = tvalues.length, mt;
+  while (j--) {
+      t = tvalues[j];
+      mt = 1 - t;
+      xvalues[j] = (mt * mt * mt * p0.x) + (3 * mt * mt * t * p1.x) + (3 * mt * t * t * p2.x) + (t * t * t * p3.x);
+      yvalues[j] = (mt * mt * mt * p0.y) + (3 * mt * mt * t * p1.y) + (3 * mt * t * t * p2.y) + (t * t * t * p3.y);
+  }
+
+  xvalues.push(p0.x,p3.x);
+  yvalues.push(p0.y,p3.y);
+
+  return {
+      x0: Math.min.apply(0, xvalues),
+      y0: Math.min.apply(0, yvalues),
+      x1: Math.max.apply(0, xvalues),
+      y1: Math.max.apply(0, yvalues)
+  };
 
 }
 
