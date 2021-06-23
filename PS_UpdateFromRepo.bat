4560ec3a5a4b41b48d9dd21e27e967405df8fd42
dpotekhin <d.potehin@peppers-studio.ru>
2021-06-23 21:19:03 +0300

    chore(PS_UpdateFromRepo.bat): minor fixes

--- PS_UpdateFromRepo.bat
+++ PS_UpdateFromRepo.bat
@@ -1,12 +1,14 @@
 @echo off
 setlocal
 
+echo [93m Installed Git is required. (https://git-scm.com/downloads) [0m
+
 set tempFolderName=.ps-temp
 set tempFolderAbsolutePath="%cd%\%tempFolderName%"
 ::echo %tempFolderAbsolutePath% %cd%
 ::GOTO END
 
-SET /P AREYOUSURE=[93m All "PS_" Scripts will be replaced. Are you sure (Y/[N])? [0m
+SET /P AREYOUSURE=[93m All "PS_*" files and folders will be replaced with the actual ones. Are you sure (Y/[N])? [0m
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
 rmdir /S /Q %tempFolderAbsolutePath%
@@ -16,4 +18,4 @@ git clone --branch dev https://github.com/dpotekhin/ToonBoomHarmonyScripts.git %
 robocopy %tempFolderAbsolutePath% "%cd%" /j /DCOPY:T /XD .git /XF .gitignore README.md /E /R:2 /W:5 /MT:16
 
 :END
-echo [93m COMPLETED [0m
+echo [93m COMPLETE [0m
