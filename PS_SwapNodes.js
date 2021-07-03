36dc81bd9803082cf06036263c884b7a62f4dfa6
dpotekhin <d.potekhin@peppers-studio.ru>
2021-07-03 21:45:49 +0300

    fix(PS_SwapNodes): fixed PS_SwapNodes script

--- PS_SwapNodes.js
+++ PS_SwapNodes.js
@@ -1,17 +1,26 @@
 /*
 Author: D.Potekhin (d@peppers-studio.ru)
-Version 0.1
-*/
+Version 0.210703
 
-/*
+This script allows to swap two nodes keeping their connections.
+
+Options:
+- Hold Control key to swap names of the nodes
+- Hold Shift key to disable swapping position of the nodes
+
 TODO:
-- to copy all connections from one node to another
+-  
 */
 
 function PS_SwapNodes(){
 
 	var selectedNodes = selection.selectedNodes();
 	if( selectedNodes.length != 2 ){
+		MessageBox.warning('Please select two nodes to swap them.\n\n'
+			+'Options:\n'
+			+'- Hold Control key to swap names of the nodes\n'
+			+'- Hold Shift key to disable swapping position of the nodes'
+		,0,0,0,'Error');
 		return;
 	}
 
@@ -19,95 +28,143 @@ function PS_SwapNodes(){
 	
 	scene.beginUndoRedoAccum('Swap Nodes');
 
-	var nodeDataA = getNodeData( selectedNodes[0] );
-	var nodeDataB = getNodeData( selectedNodes[1] );
+	var nodeA = selectedNodes[0];
+	var nodeB = selectedNodes[1];
 
-	setNodeLinks( nodeDataA.node, nodeDataB );
-	setNodeLinks( nodeDataB.node, nodeDataA );
+	var linksByNode = {};
 
-	// Swap positions
-	node.setCoord( nodeDataA.node, nodeDataB.x, nodeDataB.y );
-	node.setCoord( nodeDataB.node, nodeDataA.x, nodeDataA.y );
+	// try{
+		
+		collectLinks( nodeA );
+		collectLinks( nodeB );
+
+		// Sort outputs of both swapping nodes by a port index in ascending order
+		var linksByNodeNames = Object.keys(linksByNode);
+		linksByNodeNames.forEach(function(nodeName){
+			linksByNode[nodeName] = linksByNode[nodeName].sort(function( a, b ) {
+			  if ( a.dest.port < b.dest.port ){
+			    return -1;
+			  }
+			  if ( a.dest.port > b.dest.port ){
+			    return 1;
+			  }
+			  return 0;
+			});
+		});
+
+		// MessageLog.trace('linksByNode: \n'+ JSON.stringify(linksByNode,true,'  '));
+
+		// Unlinking nodes from the list of input ports starting from its end to avoid disorder of other connections
+		linksByNodeNames.forEach(function(nodeName){
+			var nodeLinks = linksByNode[nodeName];
+			for( var i=nodeLinks.length-1; i>=0; i-- ){
+				var linkData = nodeLinks[i];
+				node.unlink( linkData.dest.node, linkData.dest.port );
+			}
+		});
+
+		// Linking swapped connections
+		linksByNodeNames.forEach(function(nodeName){
+			var nodeLinks = linksByNode[nodeName];
+			nodeLinks.forEach(function(linkData){
+				
+				if( linkData.isInput ){
+					node.link(
+			        	linkData.src.node, linkData.src.port, // src
+			        	getSwapedNodeName( linkData.dest.node ), linkData.dest.port // dest - swapped node
+			      	);
+				}else{
+					node.link(
+			        	getSwapedNodeName( linkData.src.node ), linkData.src.port, // src - swapped node
+			        	linkData.dest.node, linkData.dest.port // dest
+			      	);
+				}
+
+			});
+		});
+
+		// Swap positions
+		if( !KeyModifiers.IsShiftPressed() ){
+			var nodeAx = node.coordX(nodeA);
+			var nodeAy = node.coordY(nodeA);
+	     	node.setCoord( nodeA, node.coordX(nodeB), node.coordY(nodeB) );
+	     	node.setCoord( nodeB, nodeAx, nodeAy );
+	    }
+
+     	// Swap names of the nodes if needed
+     	if( KeyModifiers.IsControlPressed() ){
+     		var pathA = getNodePathAndName( nodeA );
+     		var nodeATempName = pathA[1]+'__';
+     		var pathB = getNodePathAndName( nodeB );
+     		
+     		
+     		node.rename( nodeA, nodeATempName );
+     		node.rename( nodeB, pathA[1] );
+     		node.rename( pathA[0]+nodeATempName, pathB[1] );
+     		
+     	}
+
+	// }catch( err ){ MessageLog.trace('Catch Error: '+err); }
 
 	///
 	scene.endUndoRedoAccum();
 
-	//
-	function getNodeData( _node ){
 
-		var data = {
-			node: _node,
-			x: node.coordX(_node),
-			y: node.coordY(_node),
-			inputCount: node.numberOfInputPorts( _node ),
-			inputNodesInfo: [],
-			outputCount: node.numberOfOutputPorts( _node ),
-			outputNodesInfo: []
-		};
+	//
+	function getNodePathAndName( _node ){
+		var nodePath = _node.split('/');
+     	var nodeName = nodePath.pop();
+     	return [ nodePath.join('/')+'/', nodeName ];
+	}
+
+	//
+	function getSwapedNodeName( _node ){
+		return _node === nodeA ? nodeB : nodeA;
+	}
+
+	//
+	function getlinksByNodeItem( _node ){
+		if( !linksByNode[_node] ) linksByNode[_node] = [];
+		return linksByNode[_node];
+	}
+
+	//
+	function collectLinks( _node ){
 
 		// Get input connections
-		for( var i=0; i<data.inputCount; i++){
-			data.inputNodesInfo.push( node.srcNodeInfo( _node, i ) );
-		}
+	    var inputPortCount = node.numberOfInputPorts( _node );
+	    var linksByNodeItem = getlinksByNodeItem(_node);
 
-		// Unlink inputs
-		data.inputNodesInfo.forEach(function( inputData, portIndex ){
-			if( !inputData || !inputData.node) return;
-			node.unlink( _node, portIndex );
-		});
+	    for( var i=0; i<inputPortCount; i++){
+	      linksByNodeItem.push({
+	      	node: _node,
+	      	isInput: true,
+	        src: node.srcNodeInfo( _node, i ),
+	        dest: {
+	        	node: _node,
+	        	port: i
+	        }
+	      });
+	    }
 
-		// Get output connections
-		var outputNodes = [];
-		for( var i=0; i<data.outputCount; i++){
-			outputNodes.push( node.dstNodeInfo( _node, i, 0 ) );
-		}
-
-		outputNodes.forEach( function(outputData, portIndex ){
-
-			if( !outputData || !outputData.node) return;
-
-			var portCount = node.numberOfInputPorts( outputData.node );
-
-			for( var i=0; i<portCount; i++){
-				portData = node.srcNodeInfo( outputData.node, i );
-				// MessageLog.trace('--->'+JSON.stringify(portData,true,'  '));
-				if( !portData || portData.node !== _node ) continue;
-
-				portData.destNode = outputData.node;
-				portData.destPort = outputData.port;
-				data.outputNodesInfo.push(portData);
-			}
-		});
-
-		// Unlink outputs
-		data.outputNodesInfo.forEach(function( outputData ){
-			if( !outputData || !outputData.node) return;
-			node.unlink( outputData.destNode, outputData.port );
-		});
-
-		//
-		MessageLog.trace(_node+' >> '+JSON.stringify(data,true,'  '));
-
-		return data;
+	    // Get output connections
+	    var outputPortCount = node.numberOfOutputPorts( _node ),
+	    for( var opi=0; opi<outputPortCount; opi++){
+	      for(var opli = 0; opli < node.numberOfOutputLinks(_node, opi); opli++){
+	        // node.dstNode(_node, opi, opli);
+	        var outputNodeData = node.dstNodeInfo( _node, opi, opli );
+	        getlinksByNodeItem(outputNodeData.node).push({
+	          node: _node,
+	          isOutput: true,
+	          src: {
+	          	node: _node,
+	          	port: opi,
+	          	link: opli
+	          },
+	          dest: outputNodeData
+	        });
+	      }
+	    }
 	}
-
-
-
-	//
-	function setNodeLinks( _node, nodeData ){
-
-		// Link inputs
-		nodeData.inputNodesInfo.forEach(function( inputData, portIndex ){
-			if( !inputData || !inputData.node) return;
-			node.link( inputData.node, inputData.port, _node, portIndex  );
-		});
-
-		// Link outputs
-		nodeData.outputNodesInfo.forEach(function( outputData ){
-			if( !outputData || !outputData.node) return;
-			node.link( _node, outputData.port, outputData.destNode, outputData.destPort  );
-		});
-	}
-
 
 }
