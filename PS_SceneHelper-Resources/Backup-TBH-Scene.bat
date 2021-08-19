a32e323a6fe3aa2721a534ea813da1aaa833a9c6
dpotekhin <d.potehin@peppers-studio.ru>
2021-08-19 11:27:50 +0300

    fix(PS_SceneHelper): archiver changed to 7zip

--- PS_SceneHelper-Resources/Backup-TBH-Scene.bat
+++ PS_SceneHelper-Resources/Backup-TBH-Scene.bat
@@ -7,37 +7,57 @@ set fileName=%~nx1
 set filePath=%~dp1
 set filePath=%filePath:~0,-1%
 set backupPath=%filePath%\_backup
+REM set zipPath=%~dp0
+REM set zipPath=%zipPath%\..\bin\7z
+
+set curPath=%~dp0
+call "%curPath%7zip-path.bat"
 
 if not exist "%backupPath%" mkdir "%backupPath%"
 
 echo fileName^: %fileName%
 echo filePath^: %filePath%
 echo backupPath^: %backupPath%
+echo zipPath^: %zipPath%
 
-setlocal enabledelayedexpansion
+REM %zipPath% -h
+REM goto end
+
 FOR /F "skip=1 tokens=1-6" %%A IN ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Month^,Second^,Year /Format:table') DO (
     if "%%B" NEQ "" (
         SET /A FDATE=%%F*10000+%%D*100+%%A
     )
 )
 set backupStamp=%FDATE%_%time:~0,2%%time:~3,2%_%username%
-REM echo backupStamp^: %backupStamp%
+echo backupStamp^: %backupStamp%
+
+setlocal enabledelayedexpansion
+
+set fileList=
+set count=0
+for %%I IN (%*) DO (
+	set /a count+=1
+	set "fileList=!fileList! ^"!filePath!\%%~nxI^" "
+)
 
 set backupFilePath=%backupPath%\%fileName%_@%backupStamp%.zip
-REM echo backupFilePath^: %backupFilePath%
+echo backupFilePath^: %backupFilePath%
 
-set excludes=
-set excludes=--exclude=*~ --exclude=*/frames/*
+setlocal DISABLEDELAYEDEXPANSION
 
-set cmdParams=%excludes% -cvf "%backupFilePath%" -C "%filePath%" "%fileName%"
+REM set excludes
+set excludes=-xr^!*~ -xr^!*/frames/*
 
-REM echo ---------------------------------------------------
-REM echo CMD^: %cmdParams%
-REM echo ---------------------------------------------------
+set cmdParams=-tzip -ssw -mx1 -r0 -y %excludes% "%backupFilePath%" %fileList%
 
-tar %cmdParams%
+echo ---------------------------------------------------
+echo CMD^: %cmdParams%
+echo ---------------------------------------------------
+
+REM goto end
+%zipPath% a %cmdParams%
 
 echo ^<^:^<%backupFilePath%^>^:^>
 
 :end
-::pause
+REM pause
