9e521a69e367730b6ffd262aa7bee121f29c071d
dpotekhin <d.potehin@peppers-studio.ru>
2021-09-24 18:32:43 +0300

    feat(PS_KeysToDrawingSubstitutions): added Source Frame Offset and Minimum Exposure Duration settings

--- PS_KeysToDrawingSubstitutions.js
+++ PS_KeysToDrawingSubstitutions.js
@@ -1,13 +1,12 @@
 /*
 Author: D.Potekhin (d@peppers-studio.ru)
 
-Version: 0.210923
+Version: 0.210924
 */
 
 var pModal = require(fileMapper.toNativePath(specialFolders.userScripts+"/ps/pModal.js"));
 var _Utils = require(fileMapper.toNativePath(specialFolders.userScripts+"/ps/Utils.js"));
 var SelectionUtils = require(fileMapper.toNativePath(specialFolders.userScripts+"/ps/SelectionUtils.js"));
-// var NodeUtils = require(fileMapper.toNativePath(specialFolders.userScripts+"/ps/NodeUtils.js"));
 
 ///
 function PS_KeysToDrawingSubstitutions(){
@@ -16,7 +15,6 @@ function PS_KeysToDrawingSubstitutions(){
 
 	var Utils = _Utils;
 	var CUSTOM_ATTR_NAME = '_PS_KTDS_Settings';
-
 	//
   	var scriptName = 'Keys To Drawing Substitutions';
   	var scriptVer = '0.210923';
@@ -24,6 +22,7 @@ function PS_KeysToDrawingSubstitutions(){
   	var drawingNode;
   	var valueSourceNode;
   	var selectedAttrName;
+	var modalParams = {};
 
   	SelectionUtils.eachSelectedNode(function(_node){
   		if( !drawingNode && node.type(_node) === 'READ' ){
@@ -50,7 +49,7 @@ function PS_KeysToDrawingSubstitutions(){
    	var selectedAttrName = valueSourceNodeAttrs[0];
 
    	//
-   	var modal = new pModal( scriptName + " v" + scriptVer, 280, 320, false );  
+   	var modal = new pModal( scriptName + " v" + scriptVer, 315, 320, false );  
 	if( !modal.ui ){return;}
 	var ui = modal.ui;
 
@@ -61,44 +60,91 @@ function PS_KeysToDrawingSubstitutions(){
 	nodeNameLabel.setStyleSheet('font-weight:bold;');
 
 	// Node keyable attributes
-	var listWidget = modal.listWidget = new QComboBox(ui);
-	listWidget.addItems( valueSourceNodeAttrs );
-	ui.mainLayout.addWidget( listWidget, 0, 0 );
-
-	//
-	listWidget["currentIndexChanged(int)"].connect( function(i){
-		// MessageLog.trace('-->'+i);
-		onAttrNameChanged( i );
+	var sourceAttrNameInput = modal.sourceAttrNameInput = new QComboBox(ui);
+	sourceAttrNameInput.addItems( valueSourceNodeAttrs );
+	ui.mainLayout.addWidget( sourceAttrNameInput, 0, 0 );
+	sourceAttrNameInput["currentIndexChanged(int)"].connect( onAttrNameChanged );
+	addModalParam( 'sourceAttrName', sourceAttrNameInput, function( v ){
+		valueSourceNodeAttrs.forEach(function( attrName, i ){
+			if( attrName === v ) sourceAttrNameInput.setCurrentIndex( i );
+		});
 	});
 
 	//
 	var valuesGroup = modal.addGroup('',ui,'grid',true);
 
+	// 1st line
 	addLabel( 'First Frame:', valuesGroup, 0, 0 );
 	var firstFrameInput = addInput( valuesGroup, 1, 1, 0, true ); // frame.current()
+	addModalParam( 'firstFrame', firstFrameInput, undefined, function(val){
+		return validateFrame( Utils.getNumber(val) || 1 );
+	});
 
 	addLabel( 'Last Frame:', valuesGroup, 2, 0 );
 	var lastFrameInput = addInput( valuesGroup, frame.numberOf(), 3, 0, true );
+	addModalParam( 'lastFrame', lastFrameInput, undefined, function(val){
+		return validateFrame( Utils.getNumber(val) || frame.numberOf() );
+	});
+
+	// 2d line
+	addLabel( 'Value Offset (fr):', valuesGroup, 0, 1 );
+	var frameOffsetInput = addInput( valuesGroup, -1, 1, 1, true );
+	addModalParam( 'frameOffset', frameOffsetInput, undefined, true );
+
+	addLabel( 'Min Exposures (fr):', valuesGroup, 2, 1 );
+	var minExpouresInput = addInput( valuesGroup, 1, 3, 1, true );
+	addModalParam( 'minExpoures', minExpouresInput, undefined, function( val ){
+		return ~~Utils.getNumber( val ) || 1;
+	});
 
 	//
 	var subsMappingInput = new QTextEdit();
 	ui.mainLayout.addWidget( subsMappingInput, 0, 0 );
-	subsMappingInput.plainText = getDrawingSubsList(drawingNode);
 	subsMappingInput.focusOutEvent = function( e ){
 	    //MessageLog.trace('input loose focus');
-	    saveDrawingSubsList( drawingNode );
-	   	QTextEdit.focusOutEvent(e);
+	    saveDrawingSettings( drawingNode );
+	   	try{ QTextEdit.focusOutEvent(e); }catch(err){}
 	}
+	addModalParam( 'subsMapping', subsMappingInput, undefined, function(val){
+
+		if( val ) return val;
+
+		// Fill debug settings
+	    var entries = [];
+	    
+		var elementId = node.getElementId(drawingNode);
+		if( !elementId ) return '';
+
+		var n = Drawing.numberOf( elementId );
+		for( var i=0; i<n; i++ ) {
+			entries.push( Drawing.name( elementId, i ) );
+		}
+		var entryStep = 1 / entries.length;
+		entries = entries.map( function( entry, i ){
+			return ( entryStep + entryStep * i ).toPrecision(2) +' : '+entry;
+		});
+
+		return [
+	    	"#Entry Pattern:",
+			"#<Maximum Source Value> : <Subs Name> [,<Alternate Subs Name>]",
+			'',
+			'# Start a line with "#" to ignore it.',
+			'',
+			"#Example Substitution mapping bellow:",
+	    ].concat(entries).join('\n');
+
+	});
+
+	//
+	applySavedModalParams(drawingNode);
 
 	//
 	var createButton = modal.addButton('Convert Keys to Substitutions', ui, undefined, undefined, undefined, function(){
 
 		scene.beginUndoRedoAccum('Convert Keys to Substitutions');
 
-		//
 		convertKeysToSubs();
 
-		//
 		scene.endUndoRedoAccum();
 
 	});
@@ -106,7 +152,7 @@ function PS_KeysToDrawingSubstitutions(){
 	ui.mainLayout.addStretch();
   	modal.show();
 
-  	//
+  	// - - - - - - - - - - - - -
   	//
   	function onAttrNameChanged( i ){
   		// try{ 
@@ -114,6 +160,8 @@ function PS_KeysToDrawingSubstitutions(){
   		
   		var linkedColumnName = node.linkedColumn( valueSourceNode, selectedAttrName );
   		var columnName = linkedColumnName === "" ? valueSourceNode.split('/').pop()+"_SND-AMP" : linkedColumnName;
+
+  		saveDrawingSettings();
 
   		// MessageLog.trace('onAttrNameChanged: '+i+', selectedAttrName:'+selectedAttrName+', linkedColumnName:'+linkedColumnName+', columnName:'+columnName );
 
@@ -141,60 +189,104 @@ function PS_KeysToDrawingSubstitutions(){
 		return _input;
 	}
 
-	//
-	function validateFrame( fr ) {
-		return Math.min( frame.numberOf(), Math.max( 1, ~~fr ) );
-	}
 
 	//
-	function getDrawingSubsList( drawingNode ) {
+	function addModalParam( paramName, inputWidget, _setValue, _getValue ){
+
+		var getValue;
+
+		if( inputWidget instanceof QTextEdit ) {
+
+			getValue = function(){ return inputWidget.plainText };
+			setValue = function(v){ inputWidget.plainText = v };
+
+		}else if( inputWidget instanceof QLineEdit ) {
+
+			getValue = function(){ return inputWidget.text };
+			setValue = function(v){ inputWidget.text = v };
+
+		}else if( inputWidget instanceof QComboBox ) {
+
+			getValue = function(){ return inputWidget.currentText };
+			setValue = _setValue;
+
+		}
+
+		if( !getValue ){
+			MessageLog.trace('addModalParam: not defined inputWidget for "'+paramName+'"');
+			return;
+		}
+
+		if( _getValue === true ) _getValue = function( val ){
+			return ~~Utils.getNumber( val );
+		}
+
+		var paramObject = {
+			inputWidget: inputWidget,
+			getValue: function (){
+				var val = getValue();
+				// MessageLog.trace('1 >>> '+paramName+' = '+val );
+				if( _getValue ) {
+					val = _getValue( val );
+					paramObject.setValue( val );
+					// MessageLog.trace('2 >>> '+paramName+' = '+val );
+				}
+				return val;
+			},
+			setValue: setValue || function(){ MessageLog.trace('No setter available for Settings param "'+paramName+'"') },
+		};
+
+		modalParams[paramName] = paramObject;
+
+	}
+
+
+	//
+	function applySavedModalParams( drawingNode ) {
 		
 		if( !drawingNode ) return '';
 		// if( node.getTextAttr( drawingNode, 1, 'UI_DATA').indexOf('DrawingSubstitutionSwitcher') === -1 ){
 
-		var settings = node.getTextAttr( drawingNode, 1, CUSTOM_ATTR_NAME );
-		// MessageLog.trace('getDrawingSubsList: '+settings );
-		if( settings ) return settings;
+		var savedModalParams = node.getTextAttr( drawingNode, 1, CUSTOM_ATTR_NAME );
+		if( !savedModalParams ) return;
+		try{
+			savedModalParams = JSON.parse( savedModalParams );
+		}catch(err){ MessageLog.trace('applySavedModalParams: Error: '+err ); return; }
 
-	    var entries = [];
-	    
-		var elementId = node.getElementId(drawingNode);
-		if( !elementId ) return '';
+		// MessageLog.trace('applySavedModalParams: '+JSON.stringify( savedModalParams, true, '  ') );
 
-		var n = Drawing.numberOf( elementId );
-		for( var i=0; i<n; i++ ) {
-			entries.push( Drawing.name( elementId, i ) );
-		}
-		var entryStep = 1 / entries.length;
-		entries = entries.map( function( entry, i ){
-			return ( entryStep + entryStep * i ).toPrecision(2) +' : '+entry;
+		Object.keys( savedModalParams ).forEach(function( paramName ){
+			var modalParamObject = modalParams[paramName];
+			if( !modalParamObject ) return;
+			modalParamObject.setValue( savedModalParams[paramName] );
 		});
-
-		return [
-	    	"#Entry Pattern:",
-			"#<Maximum Source Value> : <Subs Name> [,<Alternate Subs Name>]",
-			'',
-			'# Start a line with "#" to ignore it.',
-			'',
-			"#Example Substitution mapping bellow:",
-	    ].concat(entries).join('\n');
 
 	}
 
 	//
-	function saveDrawingSubsList( drawingNode ){
+	function saveDrawingSettings( drawingNode ){
 
 		try{
-			var text = subsMappingInput.plainText;
+						
+			var params = {};
+			Object.keys(modalParams).forEach(function(paramName){
+				var paramObject = modalParams[paramName];
+				params[paramName] = paramObject.getValue();
+			});
+			
+			// MessageLog.trace('====>'+JSON.stringify(params,true,'  '));
+
+			var text = JSON.stringify(params);
 			var attr = node.getAttr( drawingNode, 1, CUSTOM_ATTR_NAME );
 			if( !attr.name() ) {
 				var success = node.createDynamicAttr( drawingNode, "STRING", CUSTOM_ATTR_NAME, CUSTOM_ATTR_NAME, false );
-				// MessageLog.trace('saveDrawingSubsList: Custom attribute "'+CUSTOM_ATTR_NAME+'" in "'+drawingNode+'" '+( success ? 'created.' : 'failed.') );
+				// MessageLog.trace('saveDrawingSettings: Custom attribute "'+CUSTOM_ATTR_NAME+'" in "'+drawingNode+'" '+( success ? 'created.' : 'failed.') );
 			}
 			node.setTextAttr( drawingNode, CUSTOM_ATTR_NAME, 1, text || '' );
-			// MessageLog.trace('saveDrawingSubsList: Settings saved:\n"'+text);
+			
+			// MessageLog.trace('saveDrawingSettings: Settings saved:\n"'+text);
 
-		}catch(err){MessageLog.trace('saveDrawingSubsList: '+err)}
+		}catch(err){MessageLog.trace('saveDrawingSettings: '+err)}
 
 	}
 
@@ -205,6 +297,7 @@ function PS_KeysToDrawingSubstitutions(){
 		try{
 		// MessageLog.clearLog(); // !!!
 
+		// Parse Drawing Substitution mapping data
 		var drawingSubsList = subsMappingInput.plainText
 			.split('\n')
 			.map(function(line){
@@ -228,14 +321,23 @@ function PS_KeysToDrawingSubstitutions(){
 			    return 0;
 			})
 		;
+
+		if( !drawingSubsList.length ){
+			MessageLog.trace('Drawing Substitution mapping data required.');
+			return;
+		}
 		// MessageLog.trace( 'drawingSubsList: '+JSON.stringify(drawingSubsList,true,'  '));
 
-		var _firstFrame = validateFrame( Utils.getNumber(firstFrameInput.text) || 1 );    	
-    	var _lastFrame = validateFrame( Utils.getNumber(lastFrameInput.text) || frame.numberOf() );
-    	var firstFrame = Math.max( Math.min( _firstFrame, _lastFrame ), 1 );
-    	var lastFrame = Math.min( Math.max( _firstFrame, _lastFrame ), frame.numberOf() );
-    	firstFrameInput.setText( firstFrame );
-    	lastFrameInput.setText( lastFrame );
+		var sceneFrames = frame.numberOf();
+		
+    	var frameOffset = modalParams['frameOffset'].getValue();
+    	var minExpoures = modalParams['minExpoures'].getValue();
+    	// MessageLog.trace('frameOffset:'+frameOffset+', minExpoures:'+minExpoures );
+
+    	var _firstFrame = modalParams['firstFrame'].getValue();
+    	var _lastFrame = modalParams['lastFrame'].getValue();
+    	var firstFrame = Math.min( _firstFrame, _lastFrame );
+    	var lastFrame = Math.max( _firstFrame, _lastFrame );
 
     	var valueSourceColumnId = node.linkedColumn( valueSourceNode, selectedAttrName );
     	// MessageLog.trace( 'valueSourceNode: "'+valueSourceNode+'", "'+selectedAttrName+'", "'+valueSourceColumnId+'"' );
@@ -246,10 +348,14 @@ function PS_KeysToDrawingSubstitutions(){
 		// MessageLog.trace( 'drawingNode: "'+drawingNode+'", "'+drawingColumnId+'"');
 
     	var prevEntry;
+    	var prevEntryDuration = 9999;
 
     	for( var _frame = firstFrame; _frame <= lastFrame; _frame++ ){
 
-    		var currentValue = column.getEntry( valueSourceColumnId, 1, _frame );
+    		var valueFrame = _frame + frameOffset;
+    		if( valueFrame < 1 || valueFrame > sceneFrames ) continue;
+
+    		var currentValue = column.getEntry( valueSourceColumnId, 1, valueFrame );
 
 			var drawingSubsListStepIndex = 0;
 			drawingSubsList.every(function(_step,i){
@@ -259,24 +365,32 @@ function PS_KeysToDrawingSubstitutions(){
 			var drawingSubsListEntries = drawingSubsList[ drawingSubsListStepIndex ][1];
 
 			// MessageLog.trace( '\n'+_frame+') '+currentValue+'; '+prevEntry+' > '+drawingSubsListStepIndex );
-			MessageLog.trace( 'frame: '+_frame+'; '+currentValue+'; '+prevEntry );
+			// MessageLog.trace( 'frame: '+_frame+'; '+currentValue+'; '+prevEntry );
 
 			column.setEntry( drawingColumnId, 1, _frame, null );
 
-			if( drawingSubsListEntries.indexOf( prevEntry ) !== -1 ) {
-			    MessageLog.trace('Same entry - skipped');
+			if( drawingSubsListEntries.indexOf( prevEntry ) !== -1 || prevEntryDuration < minExpoures ) {
+			    // MessageLog.trace('Same entry - skipped');
 			    column.fillEmptyCels( drawingColumnId, _frame, _frame+1 );
+			    prevEntryDuration++;
 			    continue;
 			}
 
 			prevEntry = drawingSubsListEntries[ ~~(Math.random() * drawingSubsListEntries.length ) ];
-			MessageLog.trace('Set entry: '+ prevEntry );
+			// MessageLog.trace('Set entry: '+ prevEntry );
 			column.setEntry( drawingColumnId, 1, _frame, prevEntry );
+			prevEntryDuration = 1;
 
     	}
 
     	}catch(err){MessageLog.trace('convertKeysToSubs: '+err)}
 
+	}
+
+
+	//
+	function validateFrame( fr ) {
+		return Math.min( frame.numberOf(), Math.max( 1, ~~fr ) );
 	}
 
 
