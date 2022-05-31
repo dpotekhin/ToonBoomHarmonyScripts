b20131aff9e3b9a0026b72744cddd11633e1a93f
dpotekhin <d.potehin@peppers-studio.ru>
2022-05-31 14:53:38 +0300

    chore: packed PS_DeformerTools

---
+++ ps_build/PS_DeformerTools/PS_DeformerTools.js
@@ -0,0 +1,241 @@
+/*
+Author: Dima Potekhin (skinion.onn@gmail.com)
+
+[Name: PS_SelectionSets :]
+[Version: 0.220531 :]
+
+[Description:
+A set of tools for working with deformers.
+Some tools has options - check out for tooltips on tool buttons.
+:]
+*/
+
+var _DeformerTools = require(fileMapper.toNativePath(specialFolders.userScripts+"/PS_DeformerTools-Resources/DeformerTools.js"));
+var _Utils = require(fileMapper.toNativePath(specialFolders.userScripts+"/PS_DeformerTools-Resources/ps/Utils.js"));
+var pModal = require(fileMapper.toNativePath(specialFolders.userScripts+"/PS_DeformerTools-Resources/ps/pModal.js"));
+var SelectionUtils = require(fileMapper.toNativePath(specialFolders.userScripts+"/PS_DeformerTools-Resources/ps/SelectionUtils.js"));
+
+///
+function PS_DeformerTools(){
+
+	//
+  MessageLog.clearLog();
+
+  //
+  var scriptName = 'Deformer Tools';
+  var scriptVer = '0.220401';
+  //
+
+  // var SETTINGS_NAME = 'PS_DEFORMER_TOOLS_SETTINGS';
+
+  var DeformerTools = _DeformerTools;
+  var Utils = _Utils;
+
+  var btnHeight = 30;
+  var modalWidth = 290;
+  var iconPath = fileMapper.toNativePath(specialFolders.userScripts+"/PS_DeformerTools-Resources/icons/");
+  var hGroupStyle = 'QGroupBox{ position: relative; border: none; padding-top:0; padding-bottom: 0; border-radius: 0;}';
+  var forceWindowInstances = true;//KeyModifiers.IsControlPressed();
+
+
+  //
+  var modal = new pModal( scriptName + " v" + scriptVer, modalWidth, 260, forceWindowInstances ? false : true );  
+  if( !modal.ui ){
+    return;
+  }
+  var ui = modal.ui;
+
+  ui.setStyleSheet( ui.styleSheet +' QPushButton{ border: none; }' );
+
+
+
+  // ==========================================================
+  // ALIGN
+  var alignGroup = modal.addGroup( 'Align Points:', ui, true, hGroupStyle);
+
+  modal.addButton( '', alignGroup, btnHeight, btnHeight,
+  	iconPath+'align-left.png',
+    function(){
+      DeformerTools.alignVertically( -1, KeyModifiers.IsShiftPressed() );
+    },
+    'Align points to the left.'
+    +'\n- Hold down the Shift key to change the resting attributes.'
+  );
+
+  modal.addButton( '', alignGroup, btnHeight, btnHeight,
+  	iconPath+'align-h-center.png',
+    function(){
+      DeformerTools.alignVertically( 0, KeyModifiers.IsShiftPressed() );
+    },
+    'Align points to the horizontal center.'
+    +'\n- Hold down the Shift key to change the resting attributes.'
+  );
+
+  modal.addButton( '', alignGroup, btnHeight, btnHeight,
+  	iconPath+'align-right.png',
+    function(){
+      DeformerTools.alignVertically( 1, KeyModifiers.IsShiftPressed() );
+    },
+    'Align points to the right.'
+    +'\n- Hold down the Shift key to change the resting attributes.'
+  );
+
+	//
+  modal.addVLine( btnHeight, alignGroup );
+
+  //
+  modal.addButton( '', alignGroup, btnHeight, btnHeight,
+  	iconPath+'align-top.png',
+    function(){
+      DeformerTools.alignHorizontally( 1, KeyModifiers.IsShiftPressed() );
+    },
+    'Align points to the top.'
+    +'\n- Hold down the Shift key to change the resting attributes.'
+   );
+
+	modal.addButton( '', alignGroup, btnHeight, btnHeight,
+  	iconPath+'align-v-center.png',
+    function(){
+      DeformerTools.alignHorizontally( 0, KeyModifiers.IsShiftPressed() );
+    },
+    'Align points to the center.'
+    +'\n- Hold down the Shift key to change the resting attributes.'
+   );
+
+  modal.addButton( '', alignGroup, btnHeight, btnHeight,
+  	iconPath+'align-bottom.png',
+    function(){
+      DeformerTools.alignHorizontally( -1, KeyModifiers.IsShiftPressed() );
+    },
+    'Align points to the bottom.'
+    +'\n- Hold down the Shift key to change the resting attributes.'
+   );  
+
+  ///
+  alignGroup.mainLayout.addStretch();
+
+
+
+  // ==========================================================
+  // CONTROL POINTS
+  var cpGroup = modal.addGroup( 'Control points:', ui, true, hGroupStyle);
+
+  modal.addButton( '', cpGroup, btnHeight, btnHeight,
+  	iconPath+'orient-points.png',
+    function(){
+      DeformerTools.orientControlPoints( undefined, KeyModifiers.IsShiftPressed(), KeyModifiers.IsControlPressed() );
+    },
+    'Orient control points to oposite points.'
+    +'\n- Hold down the Shift key to change the resting attributes.'
+    +'\n- Hold down the Control key to use entire chain of deformation.'
+  );
+
+  modal.addButton( '', cpGroup, btnHeight, btnHeight,
+  	iconPath+'points-on-thirds.png',
+    function(){
+      DeformerTools.distributeControlPoints( undefined, KeyModifiers.IsShiftPressed(), KeyModifiers.IsControlPressed() );
+    },
+    'Distribute control points on thirds.'
+    +'\n- Hold down the Shift key to change the resting attributes.'
+    +'\n- Hold down the Control key to use entire chain of deformation.'
+  );
+
+  modal.addButton( '', cpGroup, btnHeight, btnHeight,
+    iconPath+'move-around-left.png',
+    function(){
+      DeformerTools.moveDeformersAround('left');
+    },
+    'Move deformers around to the left'
+  );
+
+  modal.addButton( '', cpGroup, btnHeight, btnHeight,
+    iconPath+'move-around-right.png',
+    function(){
+      DeformerTools.moveDeformersAround('right');
+    },
+    'Move deformers around to the right'
+  );
+
+  modal.addButton( '', cpGroup, btnHeight, btnHeight,
+    iconPath+'insert-cp.png',
+    function(){
+      DeformerTools.insertControlPoint();
+    },
+    'Insert a Control point to the Deformer'
+  );
+
+  cpGroup.mainLayout.addStretch();
+
+
+
+
+  // ==========================================================
+  // Generate deformers
+  var gdGroup = modal.addGroup( 'Generate:', ui, true, hGroupStyle);
+  
+  modal.addButton( '', gdGroup, btnHeight, btnHeight,
+    iconPath+'generate-circle.png',
+    function(){
+      var artIndex = KeyModifiers.IsControlPressed() ? DeformerTools.COLORART : undefined;
+      DeformerTools.generateCircleDeformer( artIndex );
+    },
+    'Generate Circle Deformer.'
+    +'\n- Hold down the Control key to use Color Art as a source.'
+  );
+
+  modal.addButton( '', gdGroup, btnHeight, btnHeight,
+    iconPath+'generate-rectangle.png',
+    function(){
+      var artIndex = KeyModifiers.IsControlPressed() ? DeformerTools.COLORART : undefined;
+      DeformerTools.generateRectDeformer( artIndex );
+    },
+    'Generate Rectangle Deformer from Line Art.'
+    +'\n- Hold down the Control key to use Color Art as a source.'
+  );
+
+  modal.addButton( '', gdGroup, btnHeight, btnHeight,
+    iconPath+'generate-on-art.png',
+    function(){
+      var artIndex = KeyModifiers.IsControlPressed() ? DeformerTools.COLORART : undefined;
+      DeformerTools.generateArtDeformer( artIndex );
+    },
+    'Generate Deformer on Art.'
+    +'\n- Hold down the Control key to use Color Art as a source.'
+  );
+
+  gdGroup.mainLayout.addStretch();
+
+  //
+  ui.mainLayout.addStretch();
+
+  modal.show();
+
+
+}
+
+
+
+// !!!
+function PS_DeformerTools_TEST() {
+  
+  // _DeformerTools.orientControlPoints();
+  // _DeformerTools.generateCircleDeformer();
+  // _DeformerTools.generateRectDeformer();
+  // _DeformerTools.generateArtDeformer();
+  // _DeformerTools.moveDeformersAround('left');
+  // _DeformerTools.moveDeformersAround('right');
+  // _DeformerTools.insertControlPoint();
+  
+  /*
+  // !!!
+  var nodes = selection.selectedNodes();
+  
+  // MessageLog.trace('>>'+JSON.stringify( _Utils.getFullAttributeList( nodes[0], 1 ), true, '  ' ) );
+  
+  _Utils.getFullAttributeList( nodes[0], 1 ).forEach(function(attr){
+    MessageLog.trace( attr.keyword()+' ==> '+attr.textValue() );
+  })
+
+  // MessageLog.trace(nodes+' > '+node.type(nodes));
+  */
+}
---
+++ ps_build/PS_DeformerTools/Readme.md
@@ -0,0 +1,10 @@
+## PS_SelectionSets
+v0.220531
+
+### Description
+A set of tools for working with deformers.
+Some tools has options - check out for tooltips on tool buttons.
+
+### Installation:
+Copy all files from this folder to [Harmony User Scripts directory](https://docs.toonboom.com/help/harmony-20/premium/scripting/import-script.html).\
+Add the scripts "PS_DeformerTools", "PS_DeformerTools_TEST" to a panel.  
