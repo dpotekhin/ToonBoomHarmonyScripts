60a75f4bd2fe841da48022e36242d9f5e03e1b46
dpotekhin <d.potehin@peppers-studio.ru>
2022-08-11 15:54:36 +0300

    refactor(PS_DeformerTools)

--- PS_DeformerTools.js
+++ PS_DeformerTools.js
@@ -2,7 +2,7 @@
 Author: Dima Potekhin (skinion.onn@gmail.com)
 
 [Name: PS_SelectionSets :]
-[Version: 0.220809 :]
+[Version: 0.220811 :]
 
 [Description:
 A set of tools for working with deformers.
@@ -23,7 +23,7 @@ function PS_DeformerTools() {
 
     //
     var scriptName = 'Deformer Tools';
-    var scriptVer = '0.220809';
+    var scriptVer = '0.220811';
     //
 
     // var SETTINGS_NAME = 'PS_DEFORMER_TOOLS_SETTINGS';
@@ -32,7 +32,7 @@ function PS_DeformerTools() {
     var Utils = _Utils;
 
     var btnHeight = 30;
-    var modalWidth = 290;
+    var modalWidth = 320;
     var iconPath = fileMapper.toNativePath(specialFolders.userScripts + "/PS_DeformerTools-Resources/icons/");
     var hGroupStyle = 'QGroupBox{ position: relative; border: none; padding-top:0; padding-bottom: 0; border-radius: 0;}';
     var forceWindowInstances = true; //KeyModifiers.IsControlPressed();
@@ -47,7 +47,14 @@ function PS_DeformerTools() {
 
     ui.setStyleSheet(ui.styleSheet + ' QPushButton{ border: none; }');
 
+    var ENTIRE_CHAIN_MOD_TOOLTIP_TEXT = '\n- Hold down the Alt key to use entire chain of deformation.';
+    var MODIFIERS_TOOLTIP_TEXT = '\n- Hold down the Shift key to change the resting attributes only.' +
+        '\n- Hold down the Control key to change the current attributes values only.';
 
+    function getApplyMode() {
+        var applyMode = (KeyModifiers.IsShiftPressed() ? DeformerUtils.MODE_RESTING : 0) + (KeyModifiers.IsControlPressed() ? DeformerUtils.MODE_CURRENT : 0);
+        return applyMode !== 0 ? applyMode : DeformerUtils.MODE_CURRENT;
+    }
 
     // ==========================================================
     // ALIGN
@@ -56,28 +63,28 @@ function PS_DeformerTools() {
     modal.addButton('', alignGroup, btnHeight, btnHeight,
         iconPath + 'align-left.png',
         function() {
-            DeformerUtils.alignVertically(-1, KeyModifiers.IsShiftPressed());
+            DeformerUtils.alignVertically(-1, getApplyMode());
         },
         'Align points to the left.' +
-        '\n- Hold down the Shift key to change the resting attributes.'
+        MODIFIERS_TOOLTIP_TEXT
     );
 
     modal.addButton('', alignGroup, btnHeight, btnHeight,
         iconPath + 'align-h-center.png',
         function() {
-            DeformerUtils.alignVertically(0, KeyModifiers.IsShiftPressed());
+            DeformerUtils.alignVertically(0, getApplyMode());
         },
         'Align points to the horizontal center.' +
-        '\n- Hold down the Shift key to change the resting attributes.'
+        MODIFIERS_TOOLTIP_TEXT
     );
 
     modal.addButton('', alignGroup, btnHeight, btnHeight,
         iconPath + 'align-right.png',
         function() {
-            DeformerUtils.alignVertically(1, KeyModifiers.IsShiftPressed());
+            DeformerUtils.alignVertically(1, getApplyMode());
         },
         'Align points to the right.' +
-        '\n- Hold down the Shift key to change the resting attributes.'
+        MODIFIERS_TOOLTIP_TEXT
     );
 
     //
@@ -87,29 +94,43 @@ function PS_DeformerTools() {
     modal.addButton('', alignGroup, btnHeight, btnHeight,
         iconPath + 'align-top.png',
         function() {
-            DeformerUtils.alignHorizontally(1, KeyModifiers.IsShiftPressed());
+            DeformerUtils.alignHorizontally(1, getApplyMode());
         },
         'Align points to the top.' +
-        '\n- Hold down the Shift key to change the resting attributes.'
+        MODIFIERS_TOOLTIP_TEXT
     );
 
     modal.addButton('', alignGroup, btnHeight, btnHeight,
         iconPath + 'align-v-center.png',
         function() {
-            DeformerUtils.alignHorizontally(0, KeyModifiers.IsShiftPressed());
+            DeformerUtils.alignHorizontally(0, getApplyMode());
         },
         'Align points to the center.' +
-        '\n- Hold down the Shift key to change the resting attributes.'
+        MODIFIERS_TOOLTIP_TEXT
     );
 
     modal.addButton('', alignGroup, btnHeight, btnHeight,
         iconPath + 'align-bottom.png',
         function() {
-            DeformerUtils.alignHorizontally(-1, KeyModifiers.IsShiftPressed());
+            DeformerUtils.alignHorizontally(-1, getApplyMode());
         },
         'Align points to the bottom.' +
-        '\n- Hold down the Shift key to change the resting attributes.'
+        MODIFIERS_TOOLTIP_TEXT
     );
+
+    //
+    // modal.addVLine(btnHeight, alignGroup);
+
+    // modal.addButton('', alignGroup, btnHeight, btnHeight,
+    //     iconPath + 'x0.png',
+    //     function() {
+    //         var _node = selection.selectedNode(0);
+    //         if (!DeformerUtils.isDefNode(_node)) return;
+    //         DeformerUtils.applyAttrValue(_node, 'offset.x', 0, getApplyMode());
+    //     },
+    //     'Set X to 0.' +
+    //     MODIFIERS_TOOLTIP_TEXT
+    // );
 
     ///
     alignGroup.mainLayout.addStretch();
@@ -117,55 +138,55 @@ function PS_DeformerTools() {
 
 
     // ==========================================================
-    // CONTROL POINTS
+    // MODIFY
     var cpGroup = modal.addGroup('Modify:', ui, true, hGroupStyle);
 
     modal.addButton('', cpGroup, btnHeight, btnHeight,
         iconPath + 'orient-points.png',
         function() {
-            DeformerUtils.orientControlPoints(undefined, KeyModifiers.IsShiftPressed(), KeyModifiers.IsControlPressed());
+            DeformerUtils.orientControlPoints(undefined, getApplyMode(), KeyModifiers.IsAlternatePressed());
         },
         'Orient control points to oposite points.' +
-        '\n- Hold down the Shift key to change the resting attributes.' +
-        '\n- Hold down the Control key to use entire chain of deformation.'
+        MODIFIERS_TOOLTIP_TEXT +
+        ENTIRE_CHAIN_MOD_TOOLTIP_TEXT
     );
 
     modal.addButton('', cpGroup, btnHeight, btnHeight,
         iconPath + 'points-on-thirds.png',
         function() {
-            DeformerUtils.distributeControlPoints(undefined, KeyModifiers.IsShiftPressed(), KeyModifiers.IsControlPressed());
+            DeformerUtils.distributeControlPoints(undefined, getApplyMode(), KeyModifiers.IsAlternatePressed());
         },
         'Distribute control points on thirds.' +
-        '\n- Hold down the Shift key to change the resting attributes.' +
-        '\n- Hold down the Control key to use entire chain of deformation.'
+        MODIFIERS_TOOLTIP_TEXT +
+        ENTIRE_CHAIN_MOD_TOOLTIP_TEXT
     );
 
     modal.addButton('', cpGroup, btnHeight, btnHeight,
         iconPath + 'move-around-left.png',
         function() {
-            DeformerUtils.moveDeformersAround('left', KeyModifiers.IsShiftPressed());
+            DeformerUtils.moveDeformersAround('left', getApplyMode());
         },
         'Move deformers around to the left' +
-        '\n- Hold down the Shift key to change the resting attributes.'
+        MODIFIERS_TOOLTIP_TEXT
     );
 
     modal.addButton('', cpGroup, btnHeight, btnHeight,
         iconPath + 'move-around-right.png',
         function() {
-            DeformerUtils.moveDeformersAround('right', KeyModifiers.IsShiftPressed());
+            DeformerUtils.moveDeformersAround('right', getApplyMode());
         },
         'Move deformers around to the right' +
-        '\n- Hold down the Shift key to change the resting attributes.'
+        MODIFIERS_TOOLTIP_TEXT
     );
 
 
     modal.addButton('', cpGroup, btnHeight, btnHeight,
         iconPath + 'reverse-chain.png',
         function() {
-            DeformerUtils.reverseChain(KeyModifiers.IsShiftPressed());
+            DeformerUtils.reverseChain(getApplyMode());
         },
         'Reverse direction of the selected deformer chain.' +
-        '\n- Hold down the Shift key to change the resting attributes.'
+        MODIFIERS_TOOLTIP_TEXT
     );
 
     cpGroup.mainLayout.addStretch();
@@ -254,7 +275,7 @@ function PS_DeformerTools_TEST() {
     // _DeformerUtils.insertDeformerCurve();
     // _DeformerUtils.symmetrizeChain();
     // _DeformerUtils.symmetrizeCurves();
-    _DeformerUtils.reverseChain(KeyModifiers.IsShiftPressed());
+    _DeformerUtils.reverseChain(getApplyMode());
     // _DeformerUtils.removeDeformerCurve();
 
     /*
